c5a91df82e3abfacb0479f780e16c9aa79980f09
webberk3 <36802534+webberk3@users.noreply.github.com>
2018-02-24 14:21:14 -0800

    Add files via upload

---
+++ data/USA_PopPlacecopy_1.js
@@ -0,0 +1,1 @@
+var json_USA_PopPlacecopy_1 = {"type":"FeatureCollection","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"name":"Washington, D.C.","pop_max":4338000.0},"geometry":{"type":"Point","coordinates":[-77.011364439437159,38.901495235087012]}}]}
